542ce35fad92702c4fced8ca667a6f567b5b39b6
Luis Fernando López Romo <59634716+Romo43@users.noreply.github.com>
2021-11-12 22:48:39 -0500

    Nothing

--- server/src/config.js
+++ server/src/config.js
@@ -3,7 +3,7 @@ config()
 
 export default {
     MONGODB_URL: process.env.MONGODB_URL,
-    PORT: process.env.PORT || 8081,
+    PORT: process.env.PORT,
     SECRET: process.env.TOKEN,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
