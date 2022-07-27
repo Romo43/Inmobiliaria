d3abea69f149bf2ab5a60ea9d765e32a63999f99
Fernando Romo <lopezromo.fer43@gmail.com>
2022-07-27 13:28:51 -0500

    Employee updated

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,15 +1,22 @@
-import { changePassword } from "../controllers/employee.js";
 import { Router } from "express";
+import { check } from "express-validator";
+import { changePassword } from "../controllers/employee.js";
 import { verifyToken } from "../middlewares/authJWT.js";
+import { checkUserExists } from "../middlewares/dbValidators.js";
 
 // Create a new router
 const router = Router();
 
 // Middleware config
 router.use(verifyToken);
+router.use(checkUserExists);
 
 // Change password
-router.put("/change-password", changePassword);
+router.put(
+  "/change-password",
+  [check("password").withMessage("New password is required")],
+  changePassword
+);
 
 // Export the router
 export default router;
