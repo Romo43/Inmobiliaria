f65d2f0dfe2cf402c21c3645cf593b819eb93349
Fernando Romo <lopezromo.fer43@gmail.com>
2022-07-27 18:48:22 -0500

    Last configurations

--- src/routes/estate.js
+++ src/routes/estate.js
@@ -18,6 +18,8 @@ const router = Router();
 router.use(verifyToken);
 router.use(checkUserExists);
 
+// Estate search
+
 // Get all user estates
 router.get("/user_estates", allEstates);
 // Get estate by Id
@@ -27,11 +29,64 @@ router.get(
   findEstate
 );
 // Create new estate
-router.post("/create", createEstate);
+router.post(
+  "/create",
+  [
+    check("name", "Name is required").trim(),
+    check("description", "Description is required").trim(),
+    check("price", "Price is required").trim().isNumeric(),
+    check("estate_type", "Estate type is required")
+      .trim()
+      .isIn(["department", "house"]),
+    check("estate_status", "Estate status is required")
+      .trim()
+      .isIn(["sale", "rent"]),
+    check("areas", "Areas is required").trim(),
+    check("equipped", "Equipped is required").trim(),
+    check("terrain", "Terrain is required").trim().isNumeric(),
+    check("preserved", "Preserved is required").trim(),
+    check("service_room", "Service room is required").trim().isBoolean(),
+    check("rooms", "Rooms is required").trim().isNumeric(),
+    check("floors", "Floors is required").trim().isNumeric(),
+    check("parking", "Parking is required").trim().isNumeric(),
+    check("construction", "Construction is required").trim().isString(),
+    check("old_estate", "Old estate is required").trim().isString(),
+    check("bathrooms", "Bathrooms is required").trim().isNumeric(),
+    check("maintenance", "Maintenance is required").trim().isNumeric(),
+    check("coordinates", "Coordinates is required").trim(),
+    validateFields,
+  ],
+  createEstate
+);
 // Update all estate by Id
 router.patch(
   "/:id",
-  [check("id", "Id is required").trim().isMongoId(), validateFields],
+  [
+    check("id", "Id is required").trim().isMongoId(),
+    check("name", "Name is required").trim(),
+    check("description", "Description is required").trim(),
+    check("price", "Price is required").trim().isNumeric(),
+    check("estate_type", "Estate type is required")
+      .trim()
+      .isIn(["department", "house"]),
+    check("estate_status", "Estate status is required")
+      .trim()
+      .isIn(["sale", "rent"]),
+    check("areas", "Areas is required").trim(),
+    check("equipped", "Equipped is required").trim(),
+    check("terrain", "Terrain is required").trim().isNumeric(),
+    check("preserved", "Preserved is required").trim(),
+    check("service_room", "Service room is required").trim().isBoolean(),
+    check("rooms", "Rooms is required").trim().isNumeric(),
+    check("floors", "Floors is required").trim().isNumeric(),
+    check("parking", "Parking is required").trim().isNumeric(),
+    check("construction", "Construction is required").trim().isString(),
+    check("old_estate", "Old estate is required").trim().isString(),
+    check("bathrooms", "Bathrooms is required").trim().isNumeric(),
+    check("maintenance", "Maintenance is required").trim().isNumeric(),
+    check("coordinates", "Coordinates is required").trim(),
+    validateFields,
+  ],
   updateEstate
 );
 // Update estate status by Id and status
