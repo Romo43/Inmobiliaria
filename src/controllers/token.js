67d03e4efd02d0f80b0d426f5149a4c97aa7225c
Fernando Romo <lopezromo.fer43@gmail.com>
2022-08-05 12:54:16 -0500

    Tokens finished

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -1,13 +1,12 @@
 import Token from "../models/token.js";
 import User from "../models/User.js";
+import { sendEmail } from "../helper/sendEmail.js";
 
 // Generate token
 const generateToken = async (req, res) => {
   try {
     // Generate number with 6 between 100000 and 999999
     const token = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-
-    // const token = Math.floor(Math.random() * 1000000);
     const dateNow = new Date();
     const newToken = new Token({
       token: token,
@@ -15,8 +14,9 @@ const generateToken = async (req, res) => {
       // Add 5 minutes to current time
       expiresIn: dateNow.setMinutes(dateNow.getMinutes() + 5),
     });
+    await sendEmail(newToken.token, req.userPrimaryEmail);
     await newToken.save();
-    res.status(200).json({ data: newToken });
+    res.status(200).json({ message: "Token generated" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
