620243ad96950987f8b07073102d3039323929e9
Fernando Romo <lopezromo.fer43@gmail.com>
2022-08-07 15:18:01 -0500

    Fields updated

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,14 +6,15 @@ import { SECRET } from "../config/config.js";
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });    
-    if (!user) return res.status(400).json({message: "Invalid email or password"})
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
     const isPasswordValid = await User.comparePassword(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
-    const token = jwt.sign({ user }, SECRET);
-    res.status(200).json({ data: {token, user} });
+    const token = jwt.sign({ user }, SECRET, { expiresIn: "8h" });
+    res.status(200).json({ data: { token, user } });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
